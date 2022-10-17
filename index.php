d3d5336037f0e636fd5d0b09e073e1defaba0369
irmaaulia016 <109191384+irmaaulia016@users.noreply.github.com>
2022-10-17 07:30:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Pasien Rumah Sakit</title>
+</head>
+<body>
+    <h2>RUMAH SAKIT</h2>
+    <h2>SEHAT WARAS SAYUNG</h2>
+</body>
+<ul>
+    <li> <a href = "Profil.php">Profil</a></li>
+    <li> <a href = "pasien.php">Pasien</a></li>
+    <li> <a href = "tambah.php">Tambah Pasien</a></li>
+    <li> <a href = "Fasilitas.php">Fasilitas</a></li>
+</ul>
+</html>
